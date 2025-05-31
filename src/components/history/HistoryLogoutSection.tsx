6bd3fffe2020594c9c4d61a86fd891c49c56b5b2
Kir93 <kir931028@gmail.com>
2025-05-31 13:49:59 +0900

    feat: 로그아웃 버튼 클릭 시 중복 클릭 방지 및 비동기 처리 추가

--- src/components/history/HistoryLogoutSection.tsx
+++ src/components/history/HistoryLogoutSection.tsx
@@ -1,16 +1,43 @@
 'use client';
 
+import { useState } from 'react';
+
 import { Box, Button } from '@chakra-ui/react';
 import { useTranslations } from 'next-intl';
+
+import { useRouter } from 'src/i18n/routing';
 
 import useAuth from '@utils/useAuth';
 
 const HistoryLogoutSection = () => {
+  const router = useRouter();
   const t = useTranslations('auth');
   const { signOut } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClickSignOut = async () => {
+    if (isLoading) return; // 중복 클릭 방지
+    setIsLoading(true);
+    await signOut()
+      .then(() => {
+        router.replace('/');
+      })
+      .catch((error) => {
+        console.error('로그아웃 실패:', error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
   return (
     <Box w="100%" textAlign="right">
-      <Button size="lg" colorPalette="red" borderRadius="lg" onClick={signOut}>
+      <Button
+        loading={isLoading}
+        size="lg"
+        colorPalette="red"
+        borderRadius="lg"
+        onClick={onClickSignOut}
+      >
         {t('logoutTitle')}
       </Button>
     </Box>
