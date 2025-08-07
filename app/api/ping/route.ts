cb876d7e68fae38a3ff18acc3040ee324c3c021c
Kir93 <kir931028@gmail.com>
2025-08-07 22:33:06 +0900

    update response 의미 없는 값으로 변경

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -2,8 +2,8 @@ import supabase from '@config/supabase';
 
 export async function GET() {
   const { data, error } = await supabase.from('questions').select('id').limit(1);
-  if (error) {
+  if (error || !data?.length) {
     return new Response('Error fetching data', { status: 500 });
   }
-  return new Response(JSON.stringify(data), { status: 200 });
+  return new Response('ping', { status: 200 });
 }
